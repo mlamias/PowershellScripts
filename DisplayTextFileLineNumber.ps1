9cd13616f249984665b3c223795906e937e255aa
Mark J. Lamias <mlamias@yahoo.com>
2020-07-19 21:56:28 -0400

    Initial Upload of DisplayTextFileLineNumbers Script

---
+++ DisplayTextFileLineNumber.ps1
@@ -0,0 +1,15 @@
+﻿$n = 4238195
+$src = 'Path'
+$batch = 120
+$timer = [Diagnostics.Stopwatch]::StartNew()
+
+$count = 0
+Get-Content $src -ReadCount $batch -TotalCount $n | %  { 
+    $count += $_.Length
+    if ($count -ge $n ) {
+        $_[($n - $count + $_.Length - 1)]
+    }
+}
+
+$timer.Stop()
+$timer.Elapsed
