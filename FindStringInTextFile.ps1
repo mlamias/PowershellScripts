58b21b3b14acc0f6a23674898bbd18fcb7f7a834
Mark J. Lamias <mlamias@yahoo.com>
2020-08-18 15:22:50 -0400

    Upload of initial FindStringINTextFile.ps1 script

---
+++ FindStringInTextFile.ps1
@@ -0,0 +1,9 @@
+﻿$SEL = get-content C:\the_filepath_to_search\file_name.csv
+if( $SEL -imatch "string to search and match on" )
+{
+   Write-Host 'Contains String'
+}
+else
+{
+   Write-Host 'Does not contain String'
+}
